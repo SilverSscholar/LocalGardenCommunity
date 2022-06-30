d45ca76c47065ba84aadd8b3455759da51d88f9f
SilverSscholar <silversscholar@gmail.com>
2022-06-30 18:25:12 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocalGardenCommunity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
