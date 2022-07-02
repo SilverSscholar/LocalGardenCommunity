2d2ba373beea463655018ab447e1935239596ab4
SilverSscholar <silversscholar@gmail.com>
2022-07-02 17:19:12 -0400

    Still Attempting to Get Photos to work

--- Controllers/GardenClubController.cs
+++ Controllers/GardenClubController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LocalGardenCommunity.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LocalGardenCommunity.Controllers
 {
     public class GardenClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
 
+        public GardenClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var gardeningClubs = _context.GardeningClubs.ToList();
+            return View(gardeningClubs);
         }
     }
 }
