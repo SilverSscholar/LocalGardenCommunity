814ab175fb18a7bdcfac285aa0917df661140728
SilverSscholar <silversscholar@gmail.com>
2022-07-02 15:33:05 -0400

    Index

---
+++ Controllers/GardenClubController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocalGardenCommunity.Controllers
+{
+    public class GardenClubController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
