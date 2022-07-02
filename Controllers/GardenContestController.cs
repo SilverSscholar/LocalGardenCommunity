e306000d822a40cff1816d8b0ac57c8bff56f5f0
SilverSscholar <silversscholar@gmail.com>
2022-07-02 16:09:42 -0400

    Controllers, and Views

---
+++ Controllers/GardenContestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocalGardenCommunity.Controllers
+{
+    public class GardenContestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
